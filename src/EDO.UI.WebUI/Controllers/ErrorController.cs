247a4355371a12587fe2f198fbcf3110b2dff0ef
Vyacheslav <evangi.studio@hotmail.com>
2013-10-27 14:07:04 +0400

    Added SimpleMembership Auth

---
+++ src/EDO.UI.WebUI/Controllers/ErrorController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace EDO.UI.WebUI.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public ActionResult AccessDenied()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/EDO.UI.WebUI/Controllers/TestAuthController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SimpleMembershipModule;
+
+namespace EDO.UI.WebUI.Controllers
+{
+    [RoledAuthorize]
+    public class TestAuthController : Controller
+    {
+        //
+        // GET: /TestAuth/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
