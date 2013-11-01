d955db54095c6518f1b2ce714b7ec50d55dfb329
Vyacheslav Tilikov <evangi.studio@hotmail.com>
2013-11-01 17:13:57 +0400

    Добавлено АПИ для получения меню согласно роли авторизованного пользователя. Убрал в АПИ XML, только JSON

--- src/EDO.UI.WebUI/App_Start/WebApiConfig.cs
+++ src/EDO.UI.WebUI/App_Start/WebApiConfig.cs
@@ -19,6 +19,10 @@ namespace EDO.UI.WebUI
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            // Поддержка XML ответов нам пока не нужна, только JSON, только хардкор
+            var appXmlType = config.Formatters.XmlFormatter.SupportedMediaTypes.FirstOrDefault(t => t.MediaType == "application/xml");
+            config.Formatters.XmlFormatter.SupportedMediaTypes.Remove(appXmlType);
         }
     }
 }
--- src/EDO.UI.WebUI/Controllers/Api/SiteMenuController.cs
+++ src/EDO.UI.WebUI/Controllers/Api/SiteMenuController.cs
@@ -7,6 +7,7 @@ using System.Net.Http;
 using System.Web.Http;
 using System.Web.Hosting;
 using System.Xml.Linq;
+using EDO.UI.WebUI.Models;
 
 namespace EDO.UI.WebUI.Controllers.Api
 {
@@ -14,57 +15,14 @@ namespace EDO.UI.WebUI.Controllers.Api
     [ApiRoledAuthorize]
     public class SiteMenuController : ApiController
     {
-        public List<object> Get()
+        public Menu Get()
         {
-            List<object> result = new List<object>();
-
-            var path = HostingEnvironment.MapPath("~/App_Data/SiteMenu.xml");
-            var document = XDocument.Load(path);
-            var menuItems = document.Element("menu").Element("items").Elements("item");
-
             var principal = ControllerContext.RequestContext.Principal;
+            var path = HostingEnvironment.MapPath("~/App_Data/SiteMenu.xml");
+            var parser = new MenuParser(principal);
+            var menu = parser.LoadFromXMLFile(path);
 
-            foreach (var item in menuItems)
-            {
-                object newItem = null;
-
-                var roles = item.Element("roles").Elements("role");
-
-                //if(roles.Count() == 0 || !roles.Any(p => principal.IsInRole(p.ToString())))
-                //{
-                //    continue;
-                //}
-
-                var text = item.Element("text").ToString();
-
-                var subMenu = item.Element("items");
-
-                if(subMenu == null)
-                {
-                    newItem = new
-                    {
-                        Text = text,
-                        Controller = item.Element("controller").ToString(),
-                        Action = item.Element("action").ToString(),
-                        Target = item.Element("target").ToString()
-                    };
-                }
-                else
-                {
-                    newItem = new
-                    {
-                        Text = text,
-                        Items = new object()
-                    };
-                }
-
-                if (newItem != null)
-                {
-                    result.Add(newItem);
-                }
-            }
-
-            return result;
+            return menu;
         }
     }
 }
